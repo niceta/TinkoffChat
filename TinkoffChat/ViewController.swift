70866813f315974908e410fce3dbf084de112e3a
Kuznetsov Nikita Igorevich <n.kuznetsov@tinkoff.ru>
2019-02-28 21:50:05 +0300

    Initial Commit

---
+++ TinkoffChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by n.kuznetsov on 28/02/2019.
+//  Copyright © 2019 n.kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
