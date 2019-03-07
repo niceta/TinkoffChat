55f66c7c1a476a772d8312f9714253f6f5508152
Kuznetsov Nikita Igorevich <n.kuznetsov@tinkoff.ru>
2019-03-07 20:55:03 +0300

    add comments to profile vc

--- TinkoffChat/Controller/ProfileViewController.swift
+++ TinkoffChat/Controller/ProfileViewController.swift
@@ -23,15 +23,22 @@ class ProfileViewController: UIViewController {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        // print(editDescriptionButton.frame)
+        // в ините нет еще ui элементов, editDescriptionButton nil => фреймов тоже нет, ничего напечатать не выйдет
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(editDescriptionButton.frame)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         // Do any additional setup after loading the view, typically from a nib.
         let cornerRadius = setAvatarButton.frame.height / 2
         
         avatarImage.layer.cornerRadius = cornerRadius
-        //avatarImage.layer.masksToBounds = true  // 
+        //avatarImage.layer.masksToBounds = true  //
         avatarImage.clipsToBounds = true
         
         let insetsValue: CGFloat = 20
@@ -46,5 +53,11 @@ class ProfileViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        print(editDescriptionButton.frame)
+        // Размеры разные, потому что в момент вызова viewDidLoad вьюшки еще не добавлены в иерархию отображения,
+        // и значения берутся из сториборда, в котором сейчас висит 5й айфон =>
+        // корректную информацию по размерам получить нельзя. А в viewDidAppear вьюшки уже добавлены в иерархию
+        // для нужного девайса (например, iPhone X) => размеры указываются действительные
+        // Из-за этого факта не стоит производить вычислений вьюх на основе из размеров во viewDidLoad
     }
 }
